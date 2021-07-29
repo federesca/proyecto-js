df788001cad91ec2ad29b7a525cd1bf47cf647ab
Federico Resca <federesca@gmail.com>
2021-07-29 14:15:34 -0300

    Proyecto js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Desafio 1
+let nombre = prompt ("Ingrese su nombre completo")
+let edad = parseInt(prompt ("Ingrese su edad"))
+
+alert( "Bienvenido" + " " + nombre)
+
+document.write( nombre + " es momento de que aprendas nuevas herramientas");
