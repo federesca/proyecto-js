5e7f98f3b87366ac8d791bd0ee6c2a8602df6c7a
Federico Resca <federesca@gmail.com>
2021-08-02 23:39:11 -0300

    Desafio 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-// Desafio 1
+ // Desafio 1
 let nombre = prompt ("Ingrese su nombre completo")
 let edad = parseInt(prompt ("Ingrese su edad"))
 
 alert( "Bienvenido" + " " + nombre)
 
-document.write( nombre + " es momento de que aprendas nuevas herramientas");
+document.write( nombre + " es momento de que aprendas nuevas herramientas </br>");
+
+// Desafio 2
+
+let pago = prompt("Como prefieres abonar?");
+
+if(pago == 'efectivo') {
+    document.write('Tu pago en efectivo se ha realizado con exito');
+} else if( pago == 'tarjeta' ) {
+    document.write('Tu pago en tarjeta se ha realizado con exito');
+} else {
+    document.write('Tu pago ha sido rechazado');
+}
