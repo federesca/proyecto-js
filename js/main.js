0bbfcf86c490ee4219a711139617ff958778c313
Federico Resca <federesca@gmail.com>
2021-08-18 12:37:11 -0300

    Primer entrega final

--- js/main.js
+++ js/main.js
@@ -1,66 +1,75 @@
-// //Desafio Objeto
+function saludar () {
+  let saludo;
+  saludo = prompt ("Bienvenido a Napolitino, ¿Como te llamas?") ; 
+  if (saludo== "") {alert ("No nos dijiste tu nombre");}
+}
 
-// class Boxes {
-//     constructor(nombre, desc,precio,stock){
-//         this.nombre=nombre;
-//         this.desc=desc;
-//         this.precio=precio;
-//         this.stock=stock;
-//     }
-// vender(){
-//     this.stock= false;
-//     }
-// }
+saludar()
 
-// const pizza = new Prepizzita (1, "Nuestra prepizza amasada con 72hs de fermentacion",350,"../assets/prepi.jpg",10)
+class Box {
+  constructor(name, price, available, description = true) {
+    this.name = name;
+    this.price = price;
+    this.available = available;
+    this.description= description;
+  }
+}
 
-// let cantidad= prompt("Cuantas prepizzas queres?")
-
-// pizza.vender(cantidad);
-
-// document.write(`Felicitaciones por tu compra, nos quedan disponibles ${pizza.stock} pizzas`)
-
-//Desafio Arrays
-
+// cargar productos
 const boxes = [
-    {box: 'PrePizzita' , precio: 300} ,
-    {box: 'Boconna' , precio: 750},
-    {box: 'Blue' , precio: 650},
-    {box: 'Pancetona' , precio: 750},
-    {box: 'Sin Vueltas' , precio: 650},
-]
+  new Box('Boconna Box', 600,true,`Boconccinos y tomates secos`),
+  new Box('Blue Box', 800,true,`Queso azul y cebolla caramelizada`),
+  new Box('Pancetona', 500,false,`Panceta y huevo`),
+  new Box('Sin Vueltas', 600,true,`Clasica de muzza`),
+  new Box('Burra', 400,true,`Burrata y albahaca`),
+  new Box('Serrana', 300,true,`Jamon crudo y tomates hidratados`)
+];
 
-function menor () {
-    boxes.sort((p1 , p2) => {
-        if (p1.precio < p2.precio) {
-            return -1;
-        } else if (p1.precio > p2.precio) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }) 
-    
-    console.table(boxes)
+// mostrar productos
+document.write(`
+  <ul>
+    ${boxes.map((Box) => {
+      return `
+        <li class=text-center>
+          <strong class="card-title">${Box.name}</strong>
+          <ul class="card-text">
+            <li>Precio: ${Box.price}</li>
+            <li>${Box.description}</li>
+            <li>Stock: ${Box.available ? 'Disponible' : 'Sin Stock'}</li>
+          </ul>
+        </li>
+      `
+    }).join('')}
+    <button class="btn-warning margin left = auto margin right = auto" onclick="initBuy()">Pedime tu box</button>
+  </ul>
+`);
+// preguntar al usuario que producto quiere comprar
+function initBuy() {
+  const value = prompt('Que box desea pedir?');
+  const BoxFound = boxes.find((Box) => {
+    return Box.name === value;
+  });
+
+  if (BoxFound) {
+    document.write(`Usted eligió: ${BoxFound.name}. El precio es de ${[BoxFound.price]} mas el costo de envio`);
+  } else {
+    document.write(`No encontramos ese producto.`);
+  }
 }
 
-function mayor () {
-    boxes.sort((p1 , p2) => {
-        if (p1.precio > p2.precio) {
-            return -1;
-        } else if (p1.precio < p2.precio) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }) 
-    
-    console.table(boxes)
+function envio () {
+let lugar = prompt ('Donde enviamos tu box? (Palermo,Boedo o Caballito)');
+  if (lugar == "") { alert (`Ingresa tu barrio`);}
+  switch (lugar){
+    case "Palermo":  return 200;
+    break
+    case "Boedo": return 0;
+    break
+    case "Caballito": return 100;
+    break
+    default: alert (`No llegamos a esa zona o escribiste mal el nombre`)
+    break;
+  }
 }
-let consulta = prompt("Usted prefiere ordenar los productos desde mayor precio o desde menor precio? Ingrese mayor o menor: ")
 
-if (consulta === 'mayor') {
-    mayor()
-} else if (consulta === 'menor') {
-    menor()
-}
+alert (`El envio a esa zona te sale ${envio()} pesos. Al final de la web podes hacer tu pedido`)
